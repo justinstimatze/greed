f35c2ac270f022ff01d6626a598e9d03b6a70a4c
Justin Stimatze <justin@justinstimatze.com>
2014-05-23 09:28:34 -0400

    Add strategy switch.
    Should remove after culling old strategies, but useful for quickly
    comparing now.

--- chooseAction.js
+++ chooseAction.js
@@ -33,19 +33,25 @@ var peons = base.getByType(P);
 var peasants = base.getByType(E);
 debug ? log('Num peasants is ' + peasants.length) : null;
 var distanceWeight = 1;
+var mode = 'weighted';
 for (var peonIndex = 0; peonIndex < peons.length; peonIndex++) {
     var peon = peons[peonIndex];
-    var item = peon.getNearest(items); // Without regard to value.
-    var bestScore = -1;
-    for(var i = 0; i < items.length; ++i) {
-        var possibleItem = items[i];
-        var distance = peon.distance(possibleItem);
-        var score = possibleItem.bountyGold / (distance*distanceWeight);
-        if (score > bestScore) {
-            item = possibleItem;
-            bestScore = score;
+    var item;
+    if (mode === 'closest') {
+        item = peon.getNearest(peasents); // Without regard to value.
+    } else if (mode === 'weighted') {
+        var bestScore = -1;
+        for(var i = 0; i < items.length; ++i) {
+            var possibleItem = items[i];
+            var distance = peon.distance(possibleItem);
+            var score = possibleItem.bountyGold / (distance*distanceWeight);
+            if (score > bestScore) {
+                item = possibleItem;
+                bestScore = score;
+            }
         }
-    }
+    } 
+ 
     if (item)
         base.command(peon, 'move', item.pos);
 }
