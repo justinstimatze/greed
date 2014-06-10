f5a3a9ef0cfa35cbf8ad54668928f054dd182c80
Justin Stimatze <justin@justinstimatze.com>
2014-06-09 20:17:59 -0400

    Initial theft cleanup
    Add comments and remove unneeded sections.

--- chooseAction.js
+++ chooseAction.js
@@ -1,7 +1,6 @@
 // Init
 
-var base = this;
-var debug = false;
+var debug = true;
 var logger = '';
 function log(event) {
     logger += event + '; ';
@@ -10,36 +9,34 @@ function logVec(vec, name) {
     log(name + " = (" + Math.round(vec.x) + "," + Math.round(vec.y) + ")");
 }
 
-var FRAMES_PER_SECOND = 4.0;
+// http://discourse.codecombat.com/t/greed-tournament-game-specification-parameters-and-simulation/722
+var FRAMES_PER_SECOND = 4.0; // As provided.
 var SPEED = 10.0; // As provided.
-var MAX_DISTANCE_PER_FRAME = 2 * SPEED / FRAMES_PER_SECOND;
-var MAX_P = 5;
-
-// Trial and error.
-var ANGLE_FACTOR = 1.1 / Math.PI;
-var DISTANCE_FACTOR = 1.0;
-var VALUE_FACTOR = 0.2;
-var TEST_DISTANCE_FACTOR = 1.0;
-var FRIEND_FACTOR1 = 0.0;
-var FRIEND_FACTOR2 = 0.0;
-var ENEMY_FACTOR = 0.0;
+var MAX_DISTANCE_PER_FRAME = SPEED / FRAMES_PER_SECOND;
 
 // Shorthand variables
-var TYPES = ['peon', 'munchkin', 'ogre', 'shaman', 'fangrider', 'brawler', 'peasant'];
-var P = 'peon';
-var Pid = 0;
-var M = 'munchkin';
-var Mid = 1;
-var O = 'ogre';
-var Oid = 2;
-var S = 'shaman';
-var Sid = 3;
-var F = 'fangrider';
-var Fid = 4;
-var B = 'brawler';
-var Bid = 5;
-var E = 'peasant';
+var TYPES = ['peasant', 'soldier', 'knight', 'librarian', 'griffin-rider', 'captain', 'peon'];
+//          [ gatherer,  light,     medium,   wizard,      sniper,          heavy,     enemy]
+var Gid = 0;
+var G = TYPES[Gid];
+var Lid = 1;
+var L = TYPES[Lid];
+var Mid = 2;
+var M = TYPES[Mid];
+var WSid = 3;
+var W = TYPES[Wid];
+var Sid = 4;
+var S = TYPES[Sid];
+var Hid = 5;
+var H = TYPES[Hid];
 var Eid = 6;
+var E = TYPES[Eid];
+
+var NBINS = 4;
+var BINSIZE = 2*Math.PI/NBINS;
+
+
+var LEAD = 5.0;
 
 // Persistent values.
 if (this.now() < 0.25) {
@@ -47,239 +44,87 @@ if (this.now() < 0.25) {
         this.frames = 0;
     }
     if (typeof this.buildQueue === 'undefined') {
-        this.buildQueue = [P];
+        this.buildQueue = [G];
     }
     if (typeof this.queuedCount === 'undefined') {
-        this.queuedCount = [1, 0, 0, 0, 0, 0]; // not including enemies
+        this.queuedCount = [1, 0, 0, 0, 0, 0]; // not including most enemies
     }
-    if (typeof this.grid === 'undefined') {
-        this.grid = [];
-    }
-    if (typeof this.peons === 'undefined') {
-        this.peons = [];
-    }
-    if (typeof this.peasants === 'undefined') {
-        this.peasants = [];
-    }
-}
-
-var GRID_ROWS = 5;
-var GRID_COLS = 6;
-var GRID_MAX = 29;
-var TOP_ROW = (GRID_ROWS - 1);
-var RIGHT_COL = (GRID_COLS - 1);
-var TILE = 14;
-
-// Should be a maximum of three comparisons to get an index,
-// avoiding the division/floor cost.
-function getCell(testX, testY) {
-    var gridCol, gridRow;
-    
-    if (testX < 42) {
-        if (testX < 28) {
-             if (testX < 14) {
-                 gridCol = 0;
-             } else {
-                 gridCol = 1;
-             }
-        } else {
-           gridCol = 2;
-        }
-    } else {
-        if (testX > 56) {
-            if (testX > 70) {
-                gridCol = 5;
-            } else {
-                gridCol = 4;
-            }
-        } else {
-            gridCol = 3;
-        }
-    }
-    if (testY > 28) {
-        if (testY > 42) {
-            if (testY > 56) {
-                gridRow = 4;
-            } else {
-                gridRow = 3;
-            }
-        } else {
-            gridRow = 2;
-        }
-    } else {
-        if (testY > 14) {
-            gridRow = 1;
-        } else {
-            gridRow = 0;
-        }
+    if (typeof this.next === 'undefined') {
+        this.queuedCount = [1, 0, 0, 0, 0, 0]; // not including most enemies
     }
     
-    return [gridRow*GRID_COLS + gridCol, gridRow, gridCol];
 }
 
-debug ? log("D: " + ((this.now() * FRAMES_PER_SECOND) - this.frames)) : null;
+//debug ? log("D: " + ((this.now() * FRAMES_PER_SECOND) - this.frames)) : null;
 
 // Command
-if (this.frames % 2 === 0) {
-    var items = base.getItems();
+// Steal/starve strategy may depend on turn evaluation:
+// http://discourse.codecombat.com/t/what-is-the-turn-order-in-greed/748
 
-    // Item: Grid center point (x,y), value accumulator, empty item list.
-	this.grid = [
-	[[0.5,0.5], 0, [], 0], [[1.5,0.5], 0, [], 0], [[2.5,0.5], 0, [], 0], [[3.5,0.5], 0, [], 0], [[4.5,0.5], 0, [], 0],[[5.5,0.5], 0, [], 0],
-	[[0.5,1.5], 0, [], 0], [[1.5,1.5], 0, [], 0], [[2.5,1.5], 0, [], 0], [[3.5,1.5], 0, [], 0], [[4.5,1.5], 0, [], 0],[[5.5,1.5], 0, [], 0],
-	[[0.5,2.5], 0, [], 0], [[1.5,2.5], 0, [], 0], [[2.5,2.5], 0, [], 0], [[3.5,2.5], 0, [], 0], [[4.5,2.5], 0, [], 0],[[5.5,2.5], 0, [], 0],
-	[[0.5,3.5], 0, [], 0], [[1.5,3.5], 0, [], 0], [[2.5,3.5], 0, [], 0], [[3.5,3.5], 0, [], 0], [[4.5,3.5], 0, [], 0],[[5.5,3.5], 0, [], 0],
-	[[0.5,4.5], 0, [], 0], [[1.5,4.5], 0, [], 0], [[2.5,4.5], 0, [], 0], [[3.5,4.5], 0, [], 0], [[4.5,4.5], 0, [], 0],[[5.5,4.5], 0, [], 0],    
-	];
-    // Cheatsheet:
-    // 24,25,26,27,28,29
-    // 18,19,20,21,22,23
-    // 12,13,14,15,16,17
-    // 06,07,08,09,10,11
-    // 00,01,02,03,04,05
-    
-    // Critical loop.
-    for(var itemIndex = 0; itemIndex < items.length; ++itemIndex) {
-        
-        var testX = items[itemIndex].pos.x;
-        var testY = items[itemIndex].pos.y;
+// Assume these lists are in consistent order between turns for now, otherwise we have to sort them by id.
+// Reverse because new units seem to appear at the front.
+gatherers = this.getByType(G);
+gatherers.reverse();
+enemies = this.getByType(E);
+enemies.reverse();
 
-        var gridIndex = getCell(testX, testY)[0];
-        
-        this.grid[gridIndex][1] += items[itemIndex].bountyGold;
-        this.grid[gridIndex][2].push(items[itemIndex]);
-    }
-    
-    this.peasants = base.getByType(E);
-    for (var peasantIndex = 0; peasantIndex < this.peasants.length; ++peasantIndex) {
-        var peasantPos = this.peasants[peasantIndex].pos;
-        var peasantGridIndex = getCell(peasantPos.x, peasantPos.y)[0];
-        
-        this.grid[peasantGridIndex][1] *= ENEMY_FACTOR;
-    }
-    
-} else {
-    var peonIndexCache = [];
-    this.peons = base.getByType(P);
-    for (var peonIndex = 0; peonIndex < this.peons.length; ++peonIndex) {
-        var peonPos = this.peons[peonIndex].pos;
-        var peonGridIndex = getCell(peonPos.x, peonPos.y);
-        peonIndexCache[peonIndex] = peonGridIndex;
-        ++this.grid[peonGridIndex[0]][3];
-    }
-    
-    for (peonIndex = 0; peonIndex < this.peons.length; ++peonIndex) {
-        var peon = this.peons[peonIndex];
-        peonGridIndex = peonIndexCache[peonIndex][0];
-        peonGridRow = peonIndexCache[peonIndex][1];
-        peonGridCol = peonIndexCache[peonIndex][2];
-        var bestScore = -1;
-        var bestCellIndex;
+items = this.getItems();
 
-        for (var testCellRow = 0; testCellRow < 5; ++testCellRow) {
-            var rowDiffSq = (peonGridRow - testCellRow);
-            rowDiffSq *= rowDiffSq;
-            
-            for (var testCellCol = 0; testCellCol < 6; ++testCellCol) {
-                var colDiffSq = (peonGridCol - testCellCol);
-                colDiffSq *= colDiffSq;
-                
-                var testCellDistance = TEST_DISTANCE_FACTOR*Math.sqrt(rowDiffSq + colDiffSq);
-                
-                var testCellIndex = testCellRow*GRID_COLS + testCellCol;
-                var testCellValue = this.grid[testCellIndex][1];
-                
-                var testCellScore = testCellValue / testCellDistance;
-                
-                if (testIndex > 0 && this.grid[testCellIndex][3] > 0) {
-                    testCellScore *= FRIEND_FACTOR1;
-                }
-                if (testCellScore > bestScore) {
-                    bestScore = this.grid[testCellIndex][1];
-                    bestCellIndex = testCellIndex;
-                }
-            }
-        }
+var valueTemplate 
 
-        var pos;
-        if (bestScore > 0) {
-            var bestCell = this.grid[bestCellIndex];
-
-            var cellItems = bestCell[2];
-            var xcm = 0;
-            var ycm = 0;
-            for(var cellItemIndex = 0; cellItemIndex < cellItems.length; ++cellItemIndex) {
-                var itemInCell = cellItems[cellItemIndex];
-                xcm += itemInCell.bountyGold*itemInCell.pos.x;
-                ycm += itemInCell.bountyGold*itemInCell.pos.y;
-            }
-            xcm /= bestCell[1];
-            ycm /= bestCell[1];
- 
-            pos = new Vector(xcm, ycm);
-
-            var roughStep = Vector.subtract(pos, peon.pos);
-    
-            bestScore = -1; // Invalid
-            var bestItem; 
-            
-            var tileStepPos = Vector.add(peon.pos, Vector.limit(roughStep, TILE));
-            var tileStepIndex = getCell(tileStepPos.x, tileStepPos.y)[0];
-            var itemsEnroute = (this.grid[peonGridIndex][2]).concat(this.grid[tileStepIndex][2]);
-            for (var enrouteIndex = 0; enrouteIndex < itemsEnroute.length; ++enrouteIndex) {
-                var enrouteItem = itemsEnroute[enrouteIndex];
-                var enrouteStep = Vector.subtract(enrouteItem.pos, peon.pos);
-                var enrouteProduct = roughStep.dot(enrouteStep);
-                enrouteProduct /= (roughStep.magnitude()*enrouteStep.magnitude());
-                if (enrouteProduct > 1) { enrouteProduct = 1; } 
-                else if (enrouteProduct < -1) { enrouteProduct = -1; }
-                var enrouteAngle = ANGLE_FACTOR*Math.acos(enrouteProduct);
-                var enrouteDistance = DISTANCE_FACTOR*enrouteStep.magnitude()/roughStep.magnitude();
-                var enrouteValue = VALUE_FACTOR*enrouteItem.bountyGold;
-                var enrouteScore = enrouteValue/(enrouteAngle*enrouteDistance);
-                if (enrouteScore > bestScore) {
-                    bestScore = enrouteScore;
-                    bestItem = enrouteItem;
-                }
-            }
-            if (bestScore > 0) {
-                pos = bestItem.pos;
-            }
-
-            // For the next peon to avoid.
-            bestCell[1] *= FRIENDLY_FACTOR1; 
-            this.grid[peonGridIndex][1] *= FRIENDLY_FACTOR2;                
-        } else {
-            pos = new Vector(42.5, 35); // Map Center.
-        }
-        
-        // Protect my targetPos from multi turn spying.
-        // Also, take the smallest step possible (grab distance is 4.8ish)
-        var step = Vector.subtract(pos, peon.pos);
-        step = Vector.limit(step, MAX_DISTANCE_PER_FRAME);
-        pos = Vector.add(peon.pos, step);
-        
-        base.command(peon, 'move', pos);
-        
+// TODO: Set up value bins.
+var values = [];
+for (var enemyIndex = 0; enemyIndex < enemies.length; ++enemyIndex) {
+    var enemyVector = new Vector(enemies[enemyIndex].pos.x, enemies[enemyIndex].pos.y);
+    for (var itemIndex = 0; itemIndex < items.length; ++itemIndex {
+        var itemVector = new Vector(items[itemIndex].pos.x, items[itemIndex].pos.y);
+        var vecToItem = Vector.subtract(itemVector, enemyVector);
+        var bin = Math.floor(vecToItem.heading()/BINSIZE);
     }
 }
 
+for (var gathererIndex = 0; gathererIndex < gatherers.length; ++gathererIndex) {
+    var gatherer = gatherers[gathererIndex];
+  
+    var enemyIndex = gathererIndex % enemies.length;
+    if (enemyIndex < enemies.length) { // Avoid first frame problem.
+        var enemy = enemies[enemyIndex];
+        
+        // targetPos is too jittery to use directly.
+        //var pos = enemy.targetPos;
+        var pos = new Vector(enemy.pos.x, enemy.pos.y);
+        pos.x += LEAD;
+           
+        // Protect my targetPos from multi turn spying.
+        // Also, take the smallest step possible (grab distance is 4.8ish)
+        // var step = Vector.subtract(pos, gatherer.pos);
+        // step = Vector.limit(step, MAX_DISTANCE_PER_FRAME);
+        // pos = Vector.add(gatherer.pos, step);
+        
+        this.command(gatherer, 'move', pos);
+    }
+}
+
+
 // Build
-var expectedPeons = this.peons.length + this.queuedCount[Pid];
-debug ? log('E: ' + this.peasants.length) : null;
-debug ? log("EP: " + expectedPeons) : null;
+var expectedGatherers = gatherers.length + this.queuedCount[Gid];
+//debug ? log('E: ' + enemies.length) : null;
+//debug ? log("EG: " + expectedGatherers) : null;
 
 // Mixed force burst
-if (this.health < 300 || expectedPeons === MAX_P) {
+if (this.health < 300 || this.now() > 120) {
     if (this.gold >= 30) {
-        this.buildQueue.unshift(M);
-        this.buildQueue.unshift(M);
-        this.buildQueue.unshift(M);
+        this.buildQueue.unshift(L);
+        ++this.queuedCount[Lid];
+        this.buildQueue.unshift(L);
+        ++this.queuedCount[Lid];
+        this.buildQueue.unshift(L);
+        ++this.queuedCount[Lid];
     }
 } else {
-    if (this.gold >= 50 && expectedPeons < MAX_P) {
-        this.buildQueue.unshift(P);
-        ++this.queuedCount[Pid];
+    if (this.gold >= 50) {
+        this.buildQueue.unshift(G);
+        ++this.queuedCount[Gid];
     }
 }
 
@@ -293,8 +138,8 @@ if (this.buildQueue.length !== 0) {
         this.build(nextType);
     }
 }
-debug ? log("P: " + this.peons.length) : null;
-debug ? log("Q: " + this.buildQueue) : null;
+//debug ? log("G: " + gatherers.length) : null;
+//debug ? log("Q: " + this.buildQueue) : null;
 
 this.say(logger);
 ++this.frames;
