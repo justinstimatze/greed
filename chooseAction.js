395ffd541b33a4dcacc1c0614c90221ca62b62a2
Justin Stimatze <justin@justinstimatze.com>
2014-06-09 23:35:10 -0400

    Add prelim heading weighting
    Use item distribution and angle binning to select the
    corner we should move to relative to our enemy.

--- chooseAction.js
+++ chooseAction.js
@@ -13,10 +13,12 @@ function logVec(vec, name) {
 var FRAMES_PER_SECOND = 4.0; // As provided.
 var SPEED = 10.0; // As provided.
 var MAX_DISTANCE_PER_FRAME = SPEED / FRAMES_PER_SECOND;
+var MAX_HEALTH = 300;
 
 // Shorthand variables
-var TYPES = ['peasant', 'soldier', 'knight', 'librarian', 'griffin-rider', 'captain', 'peon'];
 //          [ gatherer,  light,     medium,   wizard,      sniper,          heavy,     enemy]
+var TYPES = ['peasant', 'soldier', 'knight', 'librarian', 'griffin-rider', 'captain', 'peon'];
+//var TYPES = ['peon', 'munchkin', 'ogre', 'shaman', 'fangrider', 'brawler', 'peasant'];
 var Gid = 0;
 var G = TYPES[Gid];
 var Lid = 1;
@@ -32,12 +34,16 @@ var H = TYPES[Hid];
 var Eid = 6;
 var E = TYPES[Eid];
 
-var NBINS = 4;
-var BINSIZE = 2*Math.PI/NBINS;
+var NUM_BINS = 4;
+var TWO_PI = 2*Math.PI;
+var BIN_SIZE = TWO_PI/NUM_BINS;
 
-
-var LEAD = 5.0;
-
+var OFFSET = 4;
+var LEADS = [[OFFSET, OFFSET],
+             [-OFFSET, OFFSET],
+             [-OFFSET, -OFFSET],
+             [OFFSET, -OFFSET]];
+             
 // Persistent values.
 if (this.now() < 0.25) {
     if (typeof this.frames === 'undefined') {
@@ -70,41 +76,52 @@ enemies.reverse();
 
 items = this.getItems();
 
-var valueTemplate 
-
-// TODO: Set up value bins.
 var values = [];
 for (var enemyIndex = 0; enemyIndex < enemies.length; ++enemyIndex) {
     var enemyVector = new Vector(enemies[enemyIndex].pos.x, enemies[enemyIndex].pos.y);
-    for (var itemIndex = 0; itemIndex < items.length; ++itemIndex {
-        var itemVector = new Vector(items[itemIndex].pos.x, items[itemIndex].pos.y);
+    values.push([[0, 0],
+                 [1, 0],
+                 [2, 0],
+                 [3, 0]]);
+
+    for (var itemIndex = 0; itemIndex < items.length; ++itemIndex) {
+        var item = items[itemIndex];
+        var itemVector = new Vector(item.pos.x, item.pos.y);
         var vecToItem = Vector.subtract(itemVector, enemyVector);
-        var bin = Math.floor(vecToItem.heading()/BINSIZE);
+        var heading = vecToItem.heading();
+        if (heading < 0) {
+            heading += TWO_PI;
+        }
+        var bin = (heading / BIN_SIZE) | 0;
+        var score = item.bountyGold / vecToItem.magnitude();
+        values[enemyIndex][bin][1] += score;
     }
+}
+
+// Result: smallest to largest, since we pop off the end.
+function byScore(a, b) {
+    return (a[1] - b[1]);
+}
+for (enemyIndex = 0; enemyIndex < enemies.length; ++enemyIndex) {
+    values[enemyIndex].sort(byScore);
 }
 
 for (var gathererIndex = 0; gathererIndex < gatherers.length; ++gathererIndex) {
     var gatherer = gatherers[gathererIndex];
   
-    var enemyIndex = gathererIndex % enemies.length;
+    enemyIndex = gathererIndex % enemies.length;
     if (enemyIndex < enemies.length) { // Avoid first frame problem.
         var enemy = enemies[enemyIndex];
         
         // targetPos is too jittery to use directly.
-        //var pos = enemy.targetPos;
         var pos = new Vector(enemy.pos.x, enemy.pos.y);
-        pos.x += LEAD;
+        var bestHeading = values[enemyIndex].pop()[0];
+        pos.x += LEADS[bestHeading][0];
+        pos.y += LEADS[bestHeading][1];
            
-        // Protect my targetPos from multi turn spying.
-        // Also, take the smallest step possible (grab distance is 4.8ish)
-        // var step = Vector.subtract(pos, gatherer.pos);
-        // step = Vector.limit(step, MAX_DISTANCE_PER_FRAME);
-        // pos = Vector.add(gatherer.pos, step);
-        
         this.command(gatherer, 'move', pos);
     }
 }
-
 
 // Build
 var expectedGatherers = gatherers.length + this.queuedCount[Gid];
@@ -112,7 +129,7 @@ var expectedGatherers = gatherers.length + this.queuedCount[Gid];
 //debug ? log("EG: " + expectedGatherers) : null;
 
 // Mixed force burst
-if (this.health < 300 || this.now() > 120) {
+if (this.health < MAX_HEALTH || this.now() > 120) {
     if (this.gold >= 30) {
         this.buildQueue.unshift(L);
         ++this.queuedCount[Lid];
